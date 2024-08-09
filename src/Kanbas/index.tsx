ee7164051c34f0497e24220de9f9f6ebd38b1f00
LucF120 <lucjferrara@gmail.com>
2024-08-09 16:13:09 -0400

    Work up to page 31 of A6

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
 import * as client from "./Courses/client";
+import Account from "./Account";
 
 export default function Kanbas() {
     const [courses, setCourses] = useState<any[]>([]);
@@ -56,7 +57,7 @@ export default function Kanbas() {
                 <div className="wd-main-content-offset p-3">
                     <Routes>
                         <Route path="/" element={<Navigate to="Dashboard" />} />
-                        <Route path="Account" element={<h1>Account</h1>} />
+                        <Route path="Account/*" element={<Account />} />
                         <Route path="Dashboard" element={
                             <Dashboard
                                 courses={courses}
