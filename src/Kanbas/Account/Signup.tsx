f91ffbb9581de1795c3ee1924334b6d21c10b896
LucF120 <lucjferrara@gmail.com>
2024-08-18 23:46:14 -0400

    Auto enroll new students in a course

--- src/Kanbas/Account/Signup.tsx
+++ src/Kanbas/Account/Signup.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import * as client from "./client";
+import * as enrollmentsClient from "../Courses/Enrollments/client";
+import * as coursesClient from "../Courses/client";
 import { useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
 export default function Signup() {
@@ -13,6 +15,13 @@ export default function Signup() {
             try {
                 const currentUser = await client.signup(user);
                 dispatch(setCurrentUser(currentUser));
+                const courses = await coursesClient.fetchAllCourses();
+                if (courses) {
+                    await enrollmentsClient.createEnrollment({
+                        user: currentUser._id,
+                        course: courses[0]._id,
+                    });
+                }
                 navigate("/Kanbas/Account/Profile");
             } catch (err: any) {
                 setError(err.response.data.message);
@@ -20,37 +29,36 @@ export default function Signup() {
         } else {
             setError("Please enter all fields");
         }
-
     };
     return (
         <div className="wd-signup-screen container">
             <h1>Sign up</h1>
             {error && <div className="wd-error alert alert-danger">{error}</div>}
             <input value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })}
-                className="wd-username form-control mb-2 w-25" placeholder="username" 
+                className="wd-username form-control mb-2 w-25" placeholder="username"
                 onKeyDown={(e) => {
-                    if (e.key === "Enter") {signup(); }
+                    if (e.key === "Enter") { signup(); }
                 }} />
             <input value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} type="password"
-                className="wd-password form-control mb-2 w-25" placeholder="password" 
+                className="wd-password form-control mb-2 w-25" placeholder="password"
                 onKeyDown={(e) => {
-                    if (e.key === "Enter") {signup(); }
+                    if (e.key === "Enter") { signup(); }
                 }} />
             <input value={user.firstName} onChange={(e) => setUser({ ...user, firstName: e.target.value })}
-                className="wd-firstname form-control mb-2 w-25" placeholder="first name" 
+                className="wd-firstname form-control mb-2 w-25" placeholder="first name"
                 onKeyDown={(e) => {
-                    if (e.key === "Enter") {signup(); }
+                    if (e.key === "Enter") { signup(); }
                 }} />
             <input value={user.lastName} onChange={(e) => setUser({ ...user, lastName: e.target.value })}
-                className="wd-firstname form-control mb-2 w-25" placeholder="last name" 
+                className="wd-firstname form-control mb-2 w-25" placeholder="last name"
                 onKeyDown={(e) => {
-                    if (e.key === "Enter") {signup(); }
+                    if (e.key === "Enter") { signup(); }
                 }} />
             <select value={user.role}
                 onChange={(e) => setUser({ ...user, role: e.target.value })}
                 className="wd-role form-select mb-2 w-25"
                 onKeyDown={(e) => {
-                    if (e.key === "Enter") {signup(); }
+                    if (e.key === "Enter") { signup(); }
                 }} >
                 <option value="STUDENT">Student</option>
                 <option value="FACULTY">Faculty</option>
